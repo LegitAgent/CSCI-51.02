148d33b02aeb5e56950305d91d479a8a501216e7
QEBS <enrique.sinjian@student.ateneo.edu>
2026-01-15 15:33:09 +0800

    added #4 code: increment letter function

--- namehash.cpp
+++ namehash.cpp
@@ -15,12 +15,31 @@ string hash_letters(string name) {
     return string(temp);
 }
 
+// function for incrementing letters
+string increment_letters(string name){  
+    int len = name.length();            // get length of name, put in variable 'len'
+    char temp[len+1];                   // create char array 'temp' with len+1 spaces
+
+    // for loop to increment each letter
+    for(int i = 0; i < len; i++){
+        char a = name[i];               // set a to be the ith char in name
+        a++;                            // increment a (turns it to b)
+        temp[i] = a;                    // set ith char in temp to be a
+    }
+    temp[len] = '\0';                   // adding null character at end of answer    
+    string answer = temp;               // creating string object 'answer' with the chars of temp
+    return answer;                      // return answer
+}
+
 int main(void) {
     string name;
     cin >> name;
 
+    string increment = increment_letters(name);
     string hashed = hash_letters(name);
-    cout <<hashed << endl;
+    cout << hashed << endl;
+    cout << increment << endl;
 
     return 0;
 }
+
