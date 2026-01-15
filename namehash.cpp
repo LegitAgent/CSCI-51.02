e8fab8d674e4417b19dd4674b6cfd7f37f17cb4d
QEBS <enrique.sinjian@student.ateneo.edu>
2026-01-15 15:49:11 +0800

    temporary commit, need to pull

--- namehash.cpp
+++ namehash.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 using namespace std;
 
-string hash_letters(string name) {
-    int len = name.length();
-    char temp[len + 1];
-    temp[0] = name[0];
 
-    for (int i = 1; i < len; i++) {
-        char a = '#';
-        temp[i] = a;
+// #3: function for replacing letters with '#'
+string hash_letters(string name) {
+    int len = name.length();            // get length of name, put in variable 'len'
+    char temp[len + 1];                 // create char array 'temp' with len+1 spaces
+    temp[0] = name[0];                  // set first letter of temp to first letter of name
+
+    // for loop to replace each succeeding letter with '#'
+    for (int i = 1; i < len; i++) {     // loops len times
+        char a = '#';                   // set char variable 'a' to '#'
+        temp[i] = a;                    // set temp value at ith position to a
     }
-    temp[len] = '\0';
+    temp[len] = '\0';                   // adding null character at end of answer
     
-    return string(temp);
+    return string(temp);                // returns string value of temp
 }
 
-// function for incrementing letters
+// #4: function for incrementing letters
 string increment_letters(string name){  
     int len = name.length();            // get length of name, put in variable 'len'
     char temp[len+1];                   // create char array 'temp' with len+1 spaces
 
     // for loop to increment each letter
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len; i++){       // loops len times
         char a = name[i];               // set a to be the ith char in name
         a++;                            // increment a (turns it to b)
         temp[i] = a;                    // set ith char in temp to be a
