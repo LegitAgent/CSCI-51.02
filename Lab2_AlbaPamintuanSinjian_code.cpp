4804224e0f797d1a72268286ee1b6cf6eba611c7
Alba Martin <albamartindarius@gmail.com>
2026-01-21 19:43:07 +0800

    did TODO for logic bug in checking ints.

--- Lab2_AlbaPamintuanSinjian_code.cpp
+++ Lab2_AlbaPamintuanSinjian_code.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+
 using namespace std;
 
 int main(){
 	int agentCount;
+    cout << "Enter Amount of Agents: " << endl;
     cin >> agentCount;
     cin.ignore(); // To ignore the newline character after reading agentCount
 
@@ -12,13 +14,17 @@ int main(){
         getline(cin, line);
         cout << "Agent #" << i << " is at (" << posX << ", " << posY << ")" << endl;
 
-        if (!isdigit(line[1]) || line[2] == ' ') { // <-- TODO: Need to correct this so that it checks if the first chararacter is not a number OR if there are more characters after a whitespace. Currently, it checks if the second character is not a number, so it fails if eg the input is "2 6 548916461954165" or 2 9 51518 48451 848484
-        
-			cout << "Agent #" << i << " yells: \"" << line.substr(1) << "\"" << endl;
-            continue;
-        } else {
+        bool isInt = true;
+        for(int j = 1; j < line.length(); j++) {
+            if(!isdigit(line[j])) {
+                isInt = false;
+                break;
+            }
+        }
+        if(isInt) {
             cout << "Agent #" << i << " has the number " << line.substr(1) << endl;
-            continue;
+        } else {
+            cout << "Agent #" << i << " yells: \"" << line.substr(1) << "\"" << endl;
         }
     }
 
