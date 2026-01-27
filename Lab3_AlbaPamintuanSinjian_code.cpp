a2d4f728c23424958883413bb8c0fede95c10bcf
QEBS <enrique.sinjian@student.ateneo.edu>
2026-01-27 22:19:30 +0800

    Initialization of quian testing

--- Lab3_AlbaPamintuanSinjian_code.cpp
+++ Lab3_AlbaPamintuanSinjian_code.cpp
@@ -5,14 +5,27 @@ using namespace std;
 int main(){
 
     // creating IntNode object i, setting values
-    IntNode *i = new IntNode();
+    IntNode *i = new IntNode;
     i->nodeData = 0;
+    IntNode *j = new IntNode;
+    j->nodeData = 1;
+    i->linkage = j;
 
     cout << i->nodeData << endl;
+    cout << i->linkage->nodeData << endl;
+
+    IntList list;
+    IntNode *dude = list.initializeHead(5);
+    IntNode *head = list.returnHead();
+    list.deleteNode(head);
+
+    cout << head->nodeData << endl;
 
     // deallocate memory
     delete i;
     i = NULL;
+    delete j;
+    j = NULL;
 
     return 0;
 }
--- Lab3_AlbaPamintuanSinjian_code.h
+++ Lab3_AlbaPamintuanSinjian_code.h
@@ -5,25 +5,55 @@ struct IntNode{
     // insert code below
     int nodeData;
     IntNode *linkage;
+
+    // constructor
+    IntNode(int nodeData, IntNode* linkage){
+        this->nodeData = nodeData;
+        this->linkage = linkage;
+    }
+
+    // destructor
+    ~IntNode(){
+        cout << "NOO BRO DIED" << cin;
+    }
 };
 
 
 struct IntList{
-    // insert code below
+    // local variables
+    IntNode *head = NULL;
+
     IntNode* initializeHead(int data) {
-        IntNode* head = new IntNode;
-        head->nodeData = data;
-        head->linkage = nullptr;
-        return head;
-    };
+        if (this->head == NULL){
+            this->head = new IntNode;
+            this->head->nodeData = data;
+            this->head->linkage = nullptr;
+        }
+        return this->head;
+    }
 
-    IntNode* insertNode(IntNode* &head, int data) {
-        IntNode* nextNode = new IntNode;
-        nextNode->nodeData = data;
-        nextNode->linkage = nullptr;
 
+    IntNode* insertNode(IntNode* head, int data) {
+        IntNode nextnode(data, NULL);
+        IntNode *next;
+        next = &nextnode;
         // TODO: point the current last node to the new node
+        
+        if(head != NULL){
+        head->linkage = next;
+        }
 
-        return nextNode; // Is this correct?
-    };
+        return next; // Is this correct?
+    }
+
+
+    IntNode* returnHead() {
+        return this->head;
+    }
+
+
+    void deleteNode(IntNode* deletus) {
+        delete deletus;
+        deletus = NULL;
+    }
 };
