e6312dc3cb18144400c0c76ab56c3c9777a196af
QEBS <enrique.sinjian@student.ateneo.edu>
2026-01-28 16:09:51 +0800

    Updated deleteNode function to link previous node to next node

--- Lab3_AlbaPamintuanSinjian_code.h
+++ Lab3_AlbaPamintuanSinjian_code.h
@@ -23,6 +23,7 @@ struct IntList{
     // local variables
     IntNode *head = NULL;
 
+    // create head node (first node)
     IntNode* initializeHead(int data) {
         if (this->head == NULL){
             this->head = new IntNode(data, NULL);
@@ -30,9 +31,10 @@ struct IntList{
         return this->head;
     }
 
+    // insert node after ANY node
     IntNode* insertNode(IntNode* existingNode, int data) {
-        IntNode *newNode = new IntNode(data, NULL);
-        newNode->linkage = existingNode->linkage; // points newNode to the next node, else segmentation fault
+        IntNode *newNode = new IntNode(data, NULL);     // creates new node
+        newNode->linkage = existingNode->linkage;       // points newNode to the next node, else segmentation fault
         existingNode->linkage = newNode;
         return newNode;
     }
@@ -59,10 +61,15 @@ struct IntList{
         return NULL;
     }
 
-
-    void deleteNode(IntNode* deletus) {
-        delete deletus;
-        deletus = NULL;
+    // deletes node
+    void deleteNode(IntNode* head) {
+        IntNode *prev = this->head;     
+        while (prev->linkage != head){
+            prev = prev->linkage;
+        }
+        prev->linkage = head->linkage;
+        delete head;
+        head = NULL;
     }
 };
 
