82fa40c6e237642b96ea0ad38b2af380d9305194
Alba Martin <albamartindarius@gmail.com>
2026-03-20 00:00:04 +0800

    added instructions 1, 2, and 3

--- mytimer.cpp
+++ mytimer.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 #include <unistd.h>
+#include <ctime>
+#include <string>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+// Function to get current time as a formatted string
+const string currentDateTime() {
+    time_t now = time(0);
+    struct tm tstruct; // tm structure
+    char buffer[22]; // 1 + 4 + 1 + 2 + 1 + 2 + 1 + 1 + 2 + 1 + 2 + 1 + 2 + 1 (null terminator "/0") = 22
+    tstruct = *localtime(&now);
+    // Format: [yyyy-mm-dd] hh:mm:ss
+    strftime(buffer, sizeof(buffer), "[%Y-%m-%d] %H:%M:%S", &tstruct);
+    return buffer;
+}
+
 int main() {
-	//insert code below
-	return 0;
+    pid_t pid1, pid2;
+    pid1 = fork();
+    if(pid1 == 0) {
+        // first child
+        const char* path = "/usr/bin/xclock";
+        char* argv[] = { (char*)"myXclock", (char*)"-digital", NULL };
+        if(execv(path, argv) == -1) {
+            cout << "Error" << endl;
+        }
+    } else {
+        pid2 = fork();
+        if(pid2 == 0) {
+            // second child
+            string str;
+            getline(cin, str);
+            cout << "”Terminated”" << endl;
+        } else {
+            // parent process
+            int counter = 0;
+            while(true) {
+                counter++;
+                cout << currentDateTime() << endl;
+
+                if(counter % 3 == 0) {
+                    cout << "”This program has gone on for far too long. Close the myXclock window or press Enter on this window to exit.”" << endl;
+                    counter = 0;
+                }
+                sleep(3);
+
+            }
+        }
+    }
+    return 0;
 }
