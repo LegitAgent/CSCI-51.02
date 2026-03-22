ebd4609db69afedfb72959ba8745a9beb38053f5
Quian <enrique.sinjian@student.ateneo.edu>
2026-03-23 03:21:33 +0800

    Added comments

--- mytimer.cpp
+++ mytimer.cpp
@@ -20,23 +20,20 @@ const string currentDateTime() {
     return buffer;
 }
 
-
+// Method to check if one child process terminates or is killed
 void check_children_termination(pid_t first, pid_t second){
     int status;
-    pid_t dead_process = waitpid(-1, &status, WNOHANG);
+    // waitpid: wait on child process status to be updated
+    // info about waitpid retrieved from 'man waitpid'
+    pid_t dead_process = waitpid(-1, &status, WNOHANG); //WNOHANG: check status of children without pausing execution
     if(dead_process == first){
-        kill(second, SIGTERM);
-        exit(0); // added this since parent seemed not to terminate on my end
+        kill(second, SIGTERM);  // kill child process 2
+        exit(0);    // exit parent process
     }else if(dead_process == second){
-        kill(first, SIGTERM);
-        exit(0); // added this since parent seemed not to terminate on my end
+        kill(first, SIGTERM);   // kill child process 1
+        exit(0);    // exit parent process
     }
 }
-
-// bool is_parent_terminated(pid_t parent_pid){
-//     pid_t current_parent = getppid();
-//     return !(current_parent == parent_pid);
-// }
 
 int main() {
     pid_t pid1, pid2, parent_pid;
@@ -48,33 +45,33 @@ int main() {
     pid1 = fork();
     if(pid1 == 0) {
         // first child
-        prctl(PR_SET_PDEATHSIG, SIGTERM);
-        const char* path = "/usr/bin/xclock";
-        char* argv[] = { (char*)"myXclock", (char*)"-digital", NULL };
-        if(execv(path, argv) == -1) {
-            cout << "Error" << endl;
+        prctl(PR_SET_PDEATHSIG, SIGTERM);       // check if parent process terminates or killed, if so terminate
+        const char* path = "/usr/bin/xclock";   // get path to xclock
+        char* argv[] = { (char*)"myXclock", (char*)"-digital", NULL };  // get arguments for executing myXclock
+        if(execv(path, argv) == -1) {           // execute xclock
+            cout << "Error" << endl;            // error if execv returns -1
         }
     } else {
         pid2 = fork();
         if(pid2 == 0) {
             // second child
-            prctl(PR_SET_PDEATHSIG, SIGTERM);
+            prctl(PR_SET_PDEATHSIG, SIGTERM);   // check if parent process terminates or killed, if so terminate
             string str;
-            getline(cin, str);
+            getline(cin, str);  // press enter to continue code, then exit
             cout << "”Terminated”" << endl;
         } else {
             // parent process
             int counter = 0;
             while(true) {
-                check_children_termination(pid1, pid2);
+                check_children_termination(pid1, pid2); // refer to check_children_termination fucntion
                 counter++;
                 cout << currentDateTime() << endl;
 
-                if(counter % 3 == 0) {
+                if(counter % 3 == 0) {  // prompts every three times
                     cout << "”This program has gone on for far too long. Close the myXclock window or press Enter on this window to exit.”" << endl;
                     counter = 0;
                 }
-                sleep(3);
+                sleep(3);       // sleep the thread for 3 seconds
             }
         }
     }
