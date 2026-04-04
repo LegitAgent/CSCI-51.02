97705125b4f778bf7539cf409bf11939cd6bb2ae
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2026-04-05 01:52:26 +0800

    Initial Commit for lab8

---
+++ mypi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <cstdlib>
+#include <ctime>
+
+void *threadfunc(void *ptr);
+
+int main(int argc, char* argv[]){
+	return 0;
+}
+
+void *threadfunc(void *ptr){
+	// monte carlo pi function	
+}
