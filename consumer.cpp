c170178195c3e2fade52883c2458a35484f96624
quian <enrique.sinjian@student.ateneo.edu>
2026-04-26 19:43:56 +0800

    Added shared memory initialization

--- consumer.cpp
+++ consumer.cpp
@@ -11,8 +11,91 @@ overboard with the FPS values that you test your program with!
 ● (1 pt) When the user presses the Enter key, the consumer process should exit.
  */
 
-#include <iostream>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    
+    // semaphore initialization
+    int semId;
+    key_t semKey = 1234;
+    int semFlag = IPC_CREAT | 0666;
+    int nSems = 1;
+
+     semId = semget( semKey, nSems, semFlag );
+    if( semId == -1 )
+    {
+        perror( "semget" );
+        exit( 1 );
+    }
+
+    // shared memory stuff
+    // Id for the shared memory
+    int shmId;
+    key_t shmKey = 2345;
+    int shmSize = 1 << 10;  //1024 bytes
+    int shmFlags = IPC_CREAT | 0666;
+    // Pointer for the starting address of the shared memory segment.
+    char* sharedMem;
+    shmId = shmget( shmKey, shmSize, shmFlags );
+    // shmat() returns the starting address of the shared memory
+    // segment, so we assign it to sharedMem.
+    sharedMem = (char*)shmat( shmId, NULL, 0 );
+
+
+    // -- Semaphore Accessing --
+    int nOperations = 2;
+    struct sembuf sema[nOperations];
+    sema[0].sem_num = 0; // Use the first semaphore in the semaphore set
+    sema[0].sem_op = 0; // Wait if semaphore != 0
+    sema[0].sem_flg = SEM_UNDO; // See slides
+    sema[1].sem_num = 0; // Use the first semaphore in the semaphore set
+    sema[1].sem_op = 1; // Increment semaphore by 1
+    sema[1].sem_flg = SEM_UNDO | IPC_NOWAIT; // See slides
+    int opResult = semop( semId, sema, nOperations );
+    // If we successfully incremented the semaphore,
+    // we can now do stuff.
+    if( opResult != -1 )
+    {
+        printf( "Successfully incremented semaphore!\n" );
+        
+        // CRITICAL SECTION
+        // TODO: for consumer...
+        // render the frame that is in the shared memory
+        if( ((int*)sharedMem) == (int*)-1 )
+        {
+            perror( "shmop: shmat failed" );
+        }
+        else
+        {
+            // TODO: Render the frame
+        }
+
+
+        // -- Semaphore Releasing --
+        // Set number of operations to 1
+        nOperations = 1;
+        // Modify the first operation such that it
+        // now decrements the semaphore.
+        sema[0].sem_num = 0; // Use the first semaphore in the semaphore set
+        sema[0].sem_op = -1; // Decrement semaphore by 1
+        sema[0].sem_flg = SEM_UNDO | IPC_NOWAIT;
+        opResult = semop( semId, sema, nOperations );
+        if( opResult == -1 )
+        {
+            perror( "semop (decrement)" );
+        }
+        else
+        {
+            printf( "Successfully decremented semaphore!\n" );
+        }
+    }
+    else
+    {
+        perror( "semop (increment)" );
+    }
+    return 0;
 }
--- producer.cpp
+++ producer.cpp
@@ -50,76 +50,84 @@ int main(int argc, char* argv[]) {
         std::cout << "File acquired." << std::endl;
     }
 
-    // -- Semaphore Initialization --
-    // Semaphore set ID
+    // semaphore initialization
     int semId;
     key_t semKey = 1234;
-    // Flags to use upon getting the semaphore set.
-    // Normally when dealing with flags, you usually bitwise-OR
-    // flags together.
-    // IPC_CREAT - If no existing semaphore set is associated with the key, createone
-    // 0666 - Remember chmod? The 0 in front specifies that the number is in octal
     int semFlag = IPC_CREAT | 0666;
-    // Number of semaphores in the semaphore set
-    // For this example, we'll just create 1 semaphore set
     int nSems = 1;
-    // Attempt to get a semaphore set
-    // Returns -1 if there was an error.
-    semId = semget( semKey, nSems, semFlag );
+
+     semId = semget( semKey, nSems, semFlag );
     if( semId == -1 )
     {
         perror( "semget" );
         exit( 1 );
     }
+
+    // shared memory stuff
+    // Id for the shared memory
+    int shmId;
+    key_t shmKey = 2345;
+    int shmSize = 1 << 10;  //1024 bytes
+    int shmFlags = IPC_CREAT | 0666;
+    // Pointer for the starting address of the shared memory segment.
+    char* sharedMem;
+    shmId = shmget( shmKey, shmSize, shmFlags );
+    // shmat() returns the starting address of the shared memory
+    // segment, so we assign it to sharedMem.
+    sharedMem = (char*)shmat( shmId, NULL, 0 );
+
+
     // -- Semaphore Accessing --
-    // Perform 2 operations
     int nOperations = 2;
-    // Create an array of size 2 to hold
-    // the operations that we will do on the semaphore set
     struct sembuf sema[nOperations];
-    // Definition for the first operation
-    // Our first operation will be to wait for the
-    // semaphore to become 0
     sema[0].sem_num = 0; // Use the first semaphore in the semaphore set
     sema[0].sem_op = 0; // Wait if semaphore != 0
     sema[0].sem_flg = SEM_UNDO; // See slides
     sema[1].sem_num = 0; // Use the first semaphore in the semaphore set
     sema[1].sem_op = 1; // Increment semaphore by 1
     sema[1].sem_flg = SEM_UNDO | IPC_NOWAIT; // See slides
-    // Perform the operations that we defined.
-    // Will return -1 on error.
     int opResult = semop( semId, sema, nOperations );
     // If we successfully incremented the semaphore,
     // we can now do stuff.
     if( opResult != -1 )
     {
-    printf( "Successfully incremented semaphore!\n" );
-    // -- Do stuff --
-    // Do stuff here
-    // -- Semaphore Releasing --
-    // Set number of operations to 1
-    nOperations = 1;
-    // Modify the first operation such that it
-    // now decrements the semaphore.
-    sema[0].sem_num = 0; // Use the first semaphore in the semaphore set
-    sema[0].sem_op = -1; // Decrement semaphore by 1
-    sema[0].sem_flg = SEM_UNDO | IPC_NOWAIT;
-    opResult = semop( semId, sema, nOperations );
-    if( opResult == -1 )
-    {
-    perror( "semop (decrement)" );
-    }
-    else
-    {
-    printf( "Successfully decremented semaphore!\n" );
-    }
-    }
-    else
-    {
-    perror( "semop (increment)" );
-    }
-    return 0;
+        printf( "Successfully incremented semaphore!\n" );
+        
+        // CRITICAL SECTION
+        // TODO: for producer...
+        // put frame into shared memory
+        if( ((int*)sharedMem) == (int*)-1 )
+        {
+            perror( "shmop: shmat failed" );
+        }
+        else
+        {
+            // TODO: Put frame into shared memory
+        }
 
+
+        // -- Semaphore Releasing --
+        // Set number of operations to 1
+        nOperations = 1;
+        // Modify the first operation such that it
+        // now decrements the semaphore.
+        sema[0].sem_num = 0; // Use the first semaphore in the semaphore set
+        sema[0].sem_op = -1; // Decrement semaphore by 1
+        sema[0].sem_flg = SEM_UNDO | IPC_NOWAIT;
+        opResult = semop( semId, sema, nOperations );
+        if( opResult == -1 )
+        {
+            perror( "semop (decrement)" );
+        }
+        else
+        {
+            printf( "Successfully decremented semaphore!\n" );
+        }
+    }
+    else
+    {
+        perror( "semop (increment)" );
+    }
     return 0;
 
 }
